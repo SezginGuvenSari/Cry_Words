163f5753913180205a6deac2aab2ef65b994dc65
Güven Sarı <59062942+SezginGuvenSari@users.noreply.github.com>
2022-04-13 13:23:52 +0300

    Basic Game Events

--- WordsPuzzleGame/Assets/Scripts/GridSquare.cs
+++ WordsPuzzleGame/Assets/Scripts/GridSquare.cs
@@ -12,11 +12,59 @@ public class GridSquare : MonoBehaviour
 
     private SpriteRenderer _displayedImage;
 
+    private bool _selected;
+    private bool _clicked;
+    private int _index = -1;
+
+
+    public void SetIndex(int index)
+    {
+        _index = index;
+    }
+
+    public int GetIndex()
+    {
+        return _index;
+    }
     void Start()
     {
+        _selected = false;
+        _clicked = false;
         _displayedImage = GetComponent<SpriteRenderer>();
     }
 
+    private void OnEnable()
+    {
+        GameEvents.OnEnableSquareSelection += OnEnableSquareSelection;
+        GameEvents.OnDisableSquareSelection += OnDisableSquareSelection;
+        GameEvents.OnSelectSquare += SelectSquare;
+
+    }
+    private void OnDisable()
+    {
+        GameEvents.OnEnableSquareSelection -= OnEnableSquareSelection;
+        GameEvents.OnDisableSquareSelection -= OnDisableSquareSelection;
+        GameEvents.OnSelectSquare -= SelectSquare;
+    }
+
+    public void OnEnableSquareSelection()
+    {
+        _clicked = true;
+        _selected = false;
+    }
+
+    public void OnDisableSquareSelection()
+    {
+        _selected = false;
+        _clicked = false;
+    }
+    private void SelectSquare(Vector3 position)
+    {
+        if (this.gameObject.transform.position == position)
+        {
+            _displayedImage.sprite = _selectedLetterData.image;
+        }
+    }
     public void SetSprite(AlphabetData.LetterData normalLetterData, AlphabetData.LetterData selectedLetterData, AlphabetData.LetterData correctLetterData)
     {
         _normalLetterData = normalLetterData;
@@ -24,5 +72,31 @@ public class GridSquare : MonoBehaviour
         _correctLetterData = correctLetterData;
 
         GetComponent<SpriteRenderer>().sprite = normalLetterData.image;
+    }
+
+    private void OnMouseDown()
+    {
+        OnEnableSquareSelection();
+        GameEvents.EnableSquareSelectionMethod();
+        CheckSquare();
+        _displayedImage.sprite = _selectedLetterData.image;
+    }
+    private void OnMouseEnter()
+    {
+        CheckSquare();
+    }
+
+    private void OnMouseUp()
+    {
+        GameEvents.ClearSelectionMethod();
+        GameEvents.DisableSquareSelectionMethod();
+    }
+   public void CheckSquare()
+    {
+        if(_selected==false && _clicked == true)
+        {
+            _selected = true;
+            GameEvents.CheckSquareMethod(_normalLetterData.letter,gameObject.transform.position, _index);
+        }
     }
 }
